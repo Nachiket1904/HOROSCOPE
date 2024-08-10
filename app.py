77f9f28acf6d67d2c8ae43ad8fe800dce736f70b
Nachiket <kapnachi1904@gmail.com>
2024-08-10 10:37:44 +0530

    first commit

--- app.py
+++ app.py
@@ -132,12 +132,3 @@ def home():
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-
-# echo "# HOROSCOPE" >> README.md
-# git init
-# git add README.md
-# git commit -m "first commit"
-# git branch -M main
-# git remote add origin https://github.com/Nachiket1904/HOROSCOPE.git
-# git push -u origin main
